4a644cad1bc44960736b24000cf75c63390fc23b
Martina Grganic <totalnobzvz@yahoo.com>
2021-12-14 16:40:17 +0000

    Add javascript for api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-https://ci-jshint.herokuapp.com/api?api_key=tJmp-rSKUQz4-QUR1X0Px_BMAcU
+const API_KEY = "VZn-Fx2ohpPGHAs_RhP_m6IgxYA";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
+}
