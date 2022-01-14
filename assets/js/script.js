6e1643f13a8666d83bba8f348729d98139823648
Martina Grganic <totalnobzvz@yahoo.com>
2022-01-14 02:31:09 +0000

    Check expiry for api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "VZn-Fx2ohpPGHAs_RhP_m6IgxYA";
+const API_KEY = "tJmp-rSKUQz4-QUR1X0Px_BMAcU";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
@@ -13,7 +13,56 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
     }
 
 }
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+
+}
+
+
+
+
+// const API_URL = "https://ci-jshint.herokuapp.com/api";  -- must be a typo, compare these two
+// const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+// document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+// async function getStatus(e) {
+
+//     const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+//     const response = await fetch(queryString);
+
+//     const data = await response.json();
+
+//     if (response.ok) {
+//         displayStatus(data.expiry);
+//     } else {
+//         throw new Error(data.error)
+//     }
+
+//     function displayStatus(data) {
+
+//         let heading = "API Key Status";
+//         let results = `<div>Your key is valid until</div>`;
+//         results += `<div class="key-status">${data.expiry}</div>`;
+
+//         document.getElementById("resultsModalTitle").innerText = heading;
+//         document.getElementById("results-content").innerHTML = results;
+//         resultsModal.show();
+//     }
+
+// }
